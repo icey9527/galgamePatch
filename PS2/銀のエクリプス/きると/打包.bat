7382552eb3e989f53af609543aacf636f5bd1eda
icey9527 <33127620@qq.com>
2026-04-15 20:31:39 +0800

    NOTICE

--- PS2/銀のエクリプス/きると/打包.bat
+++
@@ -1,4 +0,0 @@
-copy "tak\*" "tak_chs\"
-python tak.py w tak txt_chs tak_chs
-pac p tak_chs chs\tak.pac 
-python chr.py w original\slpm_667.34 0x195ab0 chr.json chs\slpm_667.34
--- PS2/銀のエクリプス/きると/提取.bat
+++
@@ -1,2 +0,0 @@
-pac u original\tak.pac tak
-python tak.py e tak txt -t original\slpm_667.34 0x195ab0
